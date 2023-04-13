f068409d040a02e4ccda4a06c9d7245718a2c536
Alexandr770 <ExoTech@yandex.ru>
2023-04-13 12:44:00 +0300

    Задание 7

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-let userName = prompt('Name?');
-const user = {
-    Name: 'Саша'
-}
-alert(Привет, Саша);
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num -+ 1;
+num +- 1;
+alert(num);
 
 
