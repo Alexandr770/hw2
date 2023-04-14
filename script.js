f615c7f4d61c48ea110ab4b754eeede64b359f04
Alexandr770 <ExoTech@yandex.ru>
2023-04-14 15:40:47 +0300

    Всё за раз

--- script.js
+++ script.js
@@ -1,2 +1,95 @@
+    1
+
+let a=10;
+alert(a);
+
+let a=20;
+alert(a);
+
+   2
+
+let year =  2007;
+alert(year); 
+ 
+   3
+
+let name = ('Brandan');
+alert(name);  
+
+   4
+
+let a = 10;
+let b = 2;
+alert(a + b);
+alert(a - b);
+alert(a * b);
+alert(a / b)  
+
+   5
+
+let a = 2;
+let b = 5;
+let result = a ** b;
+alert (32);  
+
+   6
+
+let a = 9;
+let b = 2;
+console.log(a/b);
+alert (4.5);   
+
+   7
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num -+ 1;
+num +- 1;
+alert(num);   
+
+    8
+
+let age = (prompt('Сколько Вам лет?'));
+alert(prompt);    
+
+    9
+
+const user = {
+    name: "Aleksandr",
+    age: 18,
+    isAdmin: true,
+}   
+
+    9.1
+
+user['city of residence'] = 'Самара' 
+
+    9.2
+
+const user = {
+    height: '70in'
+}    
+
+    9.3
+
+user['city of residence'] = 'Самара' 
+delete['city of residence'];
+
+    9.4
+
+const user = 'Самара'
+let info = prompt('Какую информацию хотите узнать о пользователе?');
+const user = {
+    age: '18'
+}
+alert(user[info]);  
+
+    10
+
 let userName = prompt('Введите ваше имя','Саша');
-alert("Привет Саша " + name + "!");
+alert("Привет Саша " + name + "!");    
+   
+   
