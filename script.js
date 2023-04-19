e16b2c606cd90cb066d963870b1af32849c48774
Alexandr770 <ExoTech@yandex.ru>
2023-04-19 13:43:25 +0300

    Исправления

--- script.js
+++ script.js
@@ -1,34 +1,29 @@
-       7
 
-let num = 1;
-num += 5;
-num -= 3;
-num *= 7;
-num /= 3;
-num++;
-num--;
-alert(num);   
     
-    9.2
+    //9.2
 
-user = {
+const user = {
+    name: "Aleksandr",
+    age: 18,
+    isAdmin: true,
     height: '70in'
-}    
+}  
 
-    9.3
+    //9.3
 
 user['city of residence'] = 'Самара' 
 delete['city of residence'];
 
-    9.4
+    
+    //9.4
 
-user = 'Самара'
 let info = prompt('Какую информацию хотите узнать о пользователе?');
-const user = {
-    age: '18'
-}
 alert(user[info]);
     
-     10
+   
+    //10
+
 let userName = prompt('Введите ваше имя','Саша');
-alert(prompt);
+alert(`Привет, ${userName}!`);
+
+
