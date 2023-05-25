aba086085b92213b17fead000896dc30c497b2fa
Alexandr770 <ExoTech@yandex.ru>
2023-05-25 11:25:57 +0300

    HW 3

---
+++ HW3_script.js
@@ -0,0 +1,7 @@
+let password = 8765 ;
+let userInput = Number (prompt(`Введите пароль`));
+if (password === userInput) {
+    alert = (`Пароль введён верно`);
+
+} else 
+    alert = (`Пароль введен неправильно`)
